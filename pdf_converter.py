bf8b4c06243d310a43f36ddc7c78ebca15b51e3a
Jose Ibay <ourcatisfat@gmail.com>
2026-03-23 22:50:46 +0800

    updated

--- pdf_converter.py
+++ pdf_converter.py
@@ -1,7 +1,9 @@
 #pdf converter.py
 
+
 import pypdf
 import os
+import json
 
 path = os.getcwd()
 file  = os.path.join(path, 'samplepdef.pdf')
@@ -10,8 +12,19 @@ pdf = pypdf.PdfReader(file)
 mylist = []
 for page in pdf.pages:
     print(page.extract_text())
+    
     mylist.append(page.extract_text())
     
 with open('output.txt', 'w') as f:  
     for item in mylist:
         f.write("%s\n" % item)
+        
+#save as json
+mydict = {}
+for x, y in enumerate(mylist):
+    mydict[x] = {"page": x+1, "text": y}
+    
+with open('output.json', 'w') as f:
+    json.dump(mydict, f, indent=4)
+    
+
